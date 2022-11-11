9baee055e7a40de75494b8f58b3202bd2e4258d6
yu-ma13 <10yuma0708m-_-m@ezweb.ne.jp>
2022-11-11 19:34:05 +0900

    update MysetRegisterServlet

--- src/main/java/Servlet/MysetRegister.java
+++ src/main/java/Servlet/MysetRegister.java
@@ -1,6 +1,7 @@
-package servlet;
+package Servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -11,62 +12,100 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bo.MysetRegisterLogic;
-import dao.MysetDAO;
-import entity.Contents;
-import entity.User;
+import BO.FindMyset;
+import BO.JudgeProbabilityLogic;
+import BO.MysetRegisterLogic;
+import BO.SetProbabilityLogic;
+import BO.SetRarityLogic;
+import Entity.Contents;
+import Entity.User;
 
 @WebServlet("/MysetRegister")
 public class MysetRegister extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		User loginUser = (User) session.getAttribute("loginUser");
-		Contents contents = (Contents) session.getAttribute("contents");
-		if(loginUser != null && contents != null) {
-			RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/mysetRegister.jsp");
-			d.forward(request, response);
-		} else if(loginUser != null) {
-			RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
-			d.forward(request, response);
-		} else {
-			response.sendRedirect("/gatyaSimu/");
-		}
-	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		String name = request.getParameter("name");
 		HttpSession session = request.getSession();
 		User loginUser = (User) session.getAttribute("loginUser");
-		Contents contents = (Contents) session.getAttribute("contents");
+		String name = request.getParameter("name");
+		List<String> ra = new ArrayList<>();
+		List<String> pr = new ArrayList<>();
 		
-		if(name != null && name.length() != 0) {
-			MysetDAO dao = new MysetDAO();
-			List<String> names = dao.findName(loginUser);
-			MysetRegisterLogic MRL = new MysetRegisterLogic();
-			boolean isMRL = MRL.execute(names, name);
-			if(isMRL) {
-				dao.registerMyset(loginUser, name, contents);
-				int loop = contents.getRarity().size() - 1;
-				request.setAttribute("loop", loop);
-				request.setAttribute("name", name);
-				RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/mysetRegisterResult.jsp");
-				d.forward(request, response);
-			} else {
-				String error = "※同じマイセット名は登録出来ません";
-				request.setAttribute("errorName", error);
-				request.setAttribute("name", name);
-				RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/mysetRegister.jsp");
+		ra.add(request.getParameter("ra1"));
+		ra.add(request.getParameter("ra2"));
+		ra.add(request.getParameter("ra3"));
+		ra.add(request.getParameter("ra4"));
+		ra.add(request.getParameter("ra5"));
+		
+		pr.add(request.getParameter("pr1"));
+		pr.add(request.getParameter("pr2"));
+		pr.add(request.getParameter("pr3"));
+		pr.add(request.getParameter("pr4"));
+		pr.add(request.getParameter("pr5"));
+		
+		if(ra.get(0) != null && ra.get(0).length() != 0 
+				&& pr.get(0) != null && pr.get(0).length() != 0) {
+			SetRarityLogic SRL = new SetRarityLogic();
+			List<String> rarity = SRL.execute(ra);
+			try {
+				SetProbabilityLogic SPL = new SetProbabilityLogic();
+				List<Double> probability = SPL.execute(rarity.size(), pr);
+				JudgeProbabilityLogic JPL = new JudgeProbabilityLogic();
+				boolean isPro = JPL.execute(probability);
+				if(isPro) {
+					if(name != null && name.length() != 0) {
+						Contents contents = new Contents(rarity, probability);
+						MysetRegisterLogic MRL = new MysetRegisterLogic();
+						boolean isMRL = MRL.execute(loginUser, name, contents);
+						if(isMRL) {
+							FindMyset FM = new FindMyset();
+							List<String> mysetNames = FM.execute(loginUser);
+							session.setAttribute("mysetNames", mysetNames);
+							session.setAttribute("contents", contents);
+							request.setAttribute("name", name);
+							RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/mysetRegisterResult.jsp");
+							d.forward(request, response);
+						} else {
+							String error = "※同じマイセット名は登録出来ません";
+							request.setAttribute("errorName", error);
+							request.setAttribute("name", name);
+							request.setAttribute("ra", ra);
+							request.setAttribute("pr", pr);
+							RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
+							d.forward(request, response);
+						}
+					} else {
+						String error = "※マイセット名を入力してください";
+						request.setAttribute("error", error);
+						request.setAttribute("ra", ra);
+						request.setAttribute("pr", pr);
+						RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
+						d.forward(request, response);
+					}
+				} else {
+					String error = "※各レアリティの合計が100%ではありません";
+					request.setAttribute("error", error);
+					request.setAttribute("ra", ra);
+					request.setAttribute("pr", pr);
+					RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
+					d.forward(request, response);
+				}
+			} catch (Exception e) {
+				String error = "※入力に不備があります";
+				request.setAttribute("error", error);
+				request.setAttribute("ra", ra);
+				request.setAttribute("pr", pr);
+				RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
 				d.forward(request, response);
 			}
 		} else {
-			String error = "※マイセット名を入力してください";
+			String error = "※入力に不備があります";
 			request.setAttribute("error", error);
-			RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/mysetRegister.jsp");
+			request.setAttribute("ra", ra);
+			request.setAttribute("pr", pr);
+			RequestDispatcher d = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
 			d.forward(request, response);
 		}
 	}
